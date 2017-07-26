664daa817b0081e10c89fddc58e148e4e8b118eb
Juan Sebastian Montaño Molina <juxemburg@outlook.com>
2017-07-25 20:18:35 -0500

    Cambios en el cliente javascript que permiten consultar las notificaciones pendientes

--- Hera/wwwroot/js/Notifications/notification-manager.js
+++ Hera/wwwroot/js/Notifications/notification-manager.js
@@ -1,9 +1,14 @@
 ﻿window.addEventListener("load", () => {
+    LoadEvents();
+});
+
+function LoadEvents() {
     var elem = document.getElementById('span-notification-count');
     var icon = document.getElementById('i-notification-icon');
+    var btn = document.getElementById('btn-notifications');
     $(elem).hide();
-    var initialCount = 0;
-    var activeNotifications = (count) => {
+
+    var activateNotifications = (count) => {
         if (count) {
             $(elem).show();
             icon.innerText = 'notifications_active';
@@ -12,15 +17,50 @@
         else
             $(elem).hide();
     };
-
     var chkNotifications = () => {
         $.post("/api/Notifications/Count", (data) => {
-            activeNotifications(data.count);
+            activateNotifications(data.count);
         });
     };
+    var loaded = false;
+    var loadNotifications = getLoadNotificationsHandler();
+    btn.onclick = () => {
+        loadNotifications(); 
+    };
+
     chkNotifications();
     setInterval(chkNotifications, 15000);
-});
+    
+}
+
+function getLoadNotificationsHandler() {
+    var ul = document.getElementById('ul-notifications');
+    var fStartLoad = (start) => {
+        $(ul).empty();
+        if (start)
+            ul.appendChild(getLoadingli());
+    };
+    
+    return () => {
+        fStartLoad(true);
+        $.post("/api/Notifications/Count", (data) => {
+            alert(data);
+            fStartLoad(false);
+        });
+
+    };
+}
+
+
+function getLoadingli() {
+    var li = document.createElement('li');
+    li.setAttribute('class', 'text-center');
+
+    var img = document.createElement('img');
+    img.setAttribute('src', '/images/Ellipsis.svg');
+    li.appendChild(img);
+    return li;
+}
 
 
 
